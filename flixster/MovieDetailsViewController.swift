37e444798903322241542fdc30ac3b46a203f912
Chirag Kaudan <chiragkaudan@gmail.com>
2020-09-11 00:11:28 -0700

    Committing the implementation of the MovieGridCell/ViewController, DetailsViewController, MoviesViewController

---
+++ flixster/MovieDetailsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  MovieDetailsViewController.swift
+//  flixster
+//
+//  Created by Chirag Kaudan on 9/10/20.
+//  Copyright © 2020 fmoonlclassic. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var backdropView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    var movie: [String: Any]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+       
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        posterView.af_setImage(withURL: posterUrl!)
+        
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)
+        
+        backdropView.af_setImage(withURL: backdropUrl!)
+        
+        
+        
+        
+        
+    }
+    
+   
+}
---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Chirag Kaudan on 9/10/20.
+//  Copyright © 2020 fmoonlclassic. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
